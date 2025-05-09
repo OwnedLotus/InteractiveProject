e93f1cedf03bf99be32c8e133a7b017386ba8329
Jonah Ringdahl <Jonahringdahl@outlook.com>
2025-05-09 19:03:58 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+  var toggleMenu = document.getElementById('menu')
+  if (toggleMenu.style.display === "none") {
+    toggleMenu.style.display = "block";
+  } else {
+    toggleMenu.style.display = "none";
+  }
+}
+
+function register() {
+  var buttons = document.getElementsByName(registerButton);
+
+  for (var button in buttons) {
+  }
+}
